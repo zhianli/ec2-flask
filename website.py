8196fc82d91ee4c99800d9bf3275a83004a8dc2e
Ben <30628101+zhianli@users.noreply.github.com>
2022-04-29 17:23:32 +1000

    Update website.py

--- website.py
+++ website.py
@@ -1,14 +1,16 @@
+import socket
 from flask import Flask,request
 
 app = Flask(__name__)
+
+server_ip = socket.gethostbyname(socket.gethostname()) # https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
 
 @app.route("/")
 def index():
     client_ip = request.remote_addr # https://www.adamsmith.haus/python/answers/how-to-get-an-ip-address-using-flask-in-python
     client_port = request.environ.get('REMOTE_PORT') # https://stackoverflow.com/questions/34785337/how-to-use-flask-get-clients-port
-    server_ip = request.host.split(':')[0] # https://stackoverflow.com/questions/41024296/how-to-show-my-ip-address-on-flask
     protocol = request.environ.get('SERVER_PROTOCOL')
-    return f"<p>client: {client_ip}:{client_port} <br> server ip: {server_ip} <br> protocol: {protocol}</p>"
+    return f"<p>client ip: {client_ip} <br> client port: {client_port} <br> server ip: {server_ip} <br> protocol: {protocol}</p>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host=server_ip, port=80)
