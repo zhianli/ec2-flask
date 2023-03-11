45b5996fa41e6a4482d6c63780f9e69a92070162
Ben <30628101+zhianli@users.noreply.github.com>
2023-03-11 23:08:58 +1100

    Update website.py
    added header path

--- website.py
+++ website.py
@@ -7,15 +7,20 @@ server_ip = socket.gethostbyname(socket.gethostname()) # https://stackoverflow.c
 
 @app.before_request
 def set_global_value():
+    # set the client IP, Port and protocol
     g.client_ip = request.remote_addr # https://www.adamsmith.haus/python/answers/how-to-get-an-ip-address-using-flask-in-python
     g.client_port = request.environ.get('REMOTE_PORT') # https://stackoverflow.com/questions/34785337/how-to-use-flask-get-clients-port
     g.protocol = request.environ.get('SERVER_PROTOCOL')
-    g.request_headers = '<br>'.join([f'{header[0]}: {header[1]}' for header in request.headers.items()])
+    
+    # get the HTTP request header
+    # g.request_headers = '<br>'.join([f'{header[0]}: {header[1]}' for header in request.headers.items()])
+
+    #generic response to show client IP, Port, protocol and server IP
     g.response = f"<p>client ip: {g.client_ip} <br> client port: {g.client_port} <br> server ip: {server_ip} <br> protocol: {g.protocol} </p>"
 
 @app.route("/")
 def index():
-    response = make_response(f"{g.response} <b> HTTP Request Headers:</b> <br> {g.request_headers}")
+    response = make_response(f"{g.response}")
     return response
 
 @app.route('/<http_status>')
@@ -23,6 +28,10 @@ def return_http_status(http_status):
     response = make_response(f"{g.response} <b> Status Code:</b> {http_status}</p>")
     return response, http_status
 
+@app.route('/headers')
+def headers():
+    headers = request.headers
+    return str(headers)
 
 if __name__ == "__main__":
     app.run(host=server_ip, port=80)
