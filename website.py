0e15ca32e0ce1c07a9f24e6724fcd35cabafe25e
Ben <30628101+zhianli@users.noreply.github.com>
2023-03-11 17:55:54 +1100

    Update website.py
    added status code 200

--- website.py
+++ website.py
@@ -10,7 +10,8 @@ def index():
     client_ip = request.remote_addr # https://www.adamsmith.haus/python/answers/how-to-get-an-ip-address-using-flask-in-python
     client_port = request.environ.get('REMOTE_PORT') # https://stackoverflow.com/questions/34785337/how-to-use-flask-get-clients-port
     protocol = request.environ.get('SERVER_PROTOCOL')
-    return f"<p>client ip: {client_ip} <br> client port: {client_port} <br> server ip: {server_ip} <br> protocol: {protocol}</p>"
+    return f"<p>client ip: {client_ip} <br> client port: {client_port} <br> server ip: {server_ip} <br> protocol: {protocol}</p>", 200
+
 
 if __name__ == "__main__":
     app.run(host=server_ip, port=80)
