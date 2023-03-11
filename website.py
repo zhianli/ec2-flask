eeb00f3eeb6b87a24af223da08282efea27b64b5
Ben <30628101+zhianli@users.noreply.github.com>
2023-03-12 00:30:38 +1100

    Update website.py

--- website.py
+++ website.py
@@ -12,9 +12,6 @@ def set_global_value():
     g.client_port = request.environ.get('REMOTE_PORT') # https://stackoverflow.com/questions/34785337/how-to-use-flask-get-clients-port
     g.protocol = request.environ.get('SERVER_PROTOCOL')
     
-    # get the HTTP request header
-    # g.request_headers = '<br>'.join([f'{header[0]}: {header[1]}' for header in request.headers.items()])
-
     #generic response to show client IP, Port, protocol and server IP
     g.response = f"<p>client ip: {g.client_ip} <br> client port: {g.client_port} <br> server ip: {server_ip} <br> protocol: {g.protocol} </p>"
 
